702bd20e61c23152c219bb08f6ff33f9b063d9cf
Dharma Siemarga <58156680+DharmaHS@users.noreply.github.com>
2022-11-08 01:45:10 +0700

    finished tax implimentation

--- src/income.py
+++ src/income.py
@@ -5,31 +5,52 @@ class Tax:
         self.range = b
 
     def get_income_post_tax(self, income):
-        for x in self.rate:
-            #TODO: implement income tax
-            pass
-        return 0
+        income_post_tax = 0
+        for i in range(1, 10):
+            if self.rate[i] == 0:
+                income_post_tax += (income - self.range[i-1]) * (1-self.rate[i-1])
+                break
+            if income >= self.range[i]:
+                income_post_tax += (self.range[i] - self.range[i-1] ) * (1-self.rate[i-1])
+                pass
+            elif income < self.range[i]:
+                income_post_tax += (income - self.range[i-1]) * (1-self.rate[i-1])
+                break
+            
+        return income_post_tax
 
 class Income:
-    salary_arr = []
-
-
+    income_arr = []
+    extra_money_arr = []
+    living_cost_arr = []
+    capital_arr = []
 
     def __init__(self, s, gr, sr, tr:Tax,
                     c, misc_g, cg,
-                    lc, i, r):
+                    lc, i):
         self.annual_salary = s
         self.salary_growth_rate = gr
         self.save_rate = sr
         self.initial_capital = c
-        self.misc_growth = misc_g
+        self.misc_gain = misc_g
         self.capital_growth = cg
         self.living_cost = lc
         self.inflation = i
-        self.rent = r
         self.tax_rate = tr
 
-        for i in range(1, 60):
+        current_capital = self.initial_capital
+        for i in range(1, 61):
+            # income after tax, with amount saved
             current_income = self.annual_salary * pow(self.salary_growth_rate, i)
             current_income = self.tax_rate.get_income_post_tax(current_income)
-            self.salary_arr
+            self.income_arr.append(current_income)
+            money_after_save = current_income - current_income * self.save_rate
+            self.extra_money_arr.append(current_income * self.save_rate)
+            self.living_cost_arr.append(self.living_cost * pow(self.inflation, i))
+            
+            # capital invest to S&P 500, living expense
+            current_capital += money_after_save
+            current_capital += self.misc_gain
+            current_capital -= self.living_cost * pow(self.inflation, i)
+            current_capital *= self.capital_growth
+            self.capital_arr.append(current_capital)
--- src/main.py
+++ src/main.py
@@ -11,92 +11,71 @@ def arr_dif_bigger(arr):
         return False
 
 
-def years_to_retire(annual_salary, salary_growth_rate, tax_rate,
-                    annual_save_rate,
-                    initial_capital, misc_gain,
-                    capital_growth,
-                    living_cost, inflation, rent_cost):
+def years_to_retire(me:income.Income):
     """
     years_to_retire:
     returns: years to retire, money plot
     """
+    year_list = []
     capital_arr = []
-    living_cost_arr = []
     income_arr = []
     extra_money_arr = []
-    year_list = []
+    living_cost_arr = []
 
-    living_cost += rent_cost
-
-    
-    current_capital = initial_capital
-    for i in range(1, 60):
-        year_list.append(i)
-
-        current_salary = annual_salary * pow(salary_growth_rate, i)
-
-        income_after_tax = current_salary * (1-tax_rate)
-        income_arr.append(current_salary)
-        money_saved = income_after_tax * annual_save_rate
-        money_after_save = income_after_tax - money_saved
-        extra_money_arr.append(money_saved)
-
-        current_living_cost = living_cost * pow(inflation, i)
-        living_cost_arr.append(current_living_cost)
-
-        current_capital += misc_gain
-        current_capital += money_after_save
-        current_capital -= current_living_cost
-        current_capital *= capital_growth
-        capital_arr.append(current_capital)
-
+    for i in range(0, 60):
         future_arr = []
-        future_capital = current_capital
+        future_capital = me.capital_arr[i]
         for j in range(1,4):
-            future_living_cost  = current_living_cost * pow(inflation, j)
+            future_living_cost = me.living_cost_arr[i] * pow(me.inflation, j)
             future_capital -= future_living_cost
-            future_arr.append(future_capital * pow(capital_growth, j))
+            future_arr.append(future_capital * pow(me.capital_growth, j))
 
         if arr_dif_bigger(future_arr) == True:
+            for j in range(0, i):
+                year_list.append(j)
+                capital_arr.append(me.capital_arr[j])
+                income_arr.append(me.income_arr[j])
+                living_cost_arr.append(me.living_cost_arr[j])
+                extra_money_arr.append(me.extra_money_arr[j])
             for j in range(1, 10):
-                year_list.append(i + j)
+                year_list.append(i + j - 1)
+                capital_arr.append(capital_arr[i+j-2] * me.capital_growth - living_cost_arr[i+j-2] * me.inflation)
                 income_arr.append(0)
-                living_cost_arr.append(living_cost_arr[i+j-2] * inflation)
-                capital_arr.append(capital_arr[i+j-2] * capital_growth)
+                living_cost_arr.append(living_cost_arr[i+j-2] * me.inflation)
                 extra_money_arr.append(0)
 
-            print(i)
             plt.plot(year_list, capital_arr, label="money on investment")
             plt.plot(year_list, living_cost_arr, label="annual living cost")
             plt.plot(year_list, income_arr, label="annual work income post tax")
             plt.plot(year_list, extra_money_arr, label="annual extra money (saved to bank)")
             plt.xlabel('Years')
             plt.ylabel('CAD($)')
-            _response_year = "you can retire after "+ str(i) +" year of working"
-            plt.title(_response_year)
+            response_title = "you can retire after "+ str(i+1) +" year of working"
+            plt.title(response_title)
             plt.legend()
             plt.show()
-            return i
-
+            return i+1
+    print(-1)
     return -1
 
 
 if __name__=="__main__":
-    tax1_bracket_rate = [0.15, 0.205, 0.26, 0.29, 0.33]
-    tax1_bracket_range = [50197, 100392, 155625, 211708]
+    tax1_bracket_rate = [0.15, 0.205, 0.26, 0.29, 0.33, 0]
+    tax1_bracket_range = [0, 50197, 100392, 155625, 211708, 0]
     tax1 = income.Tax(tax1_bracket_rate, tax1_bracket_range)
     
     my_tax_rate = 0.17
 
     my_salary = 79435
     my_salary_growth = 1.03
-    my_save_rate = 0.1
+    my_save_rate = 0.2
     my_capital = 15000
     my_misc_gain = 16000
     my_capital_growth = 1.107
     my_living_cost = 33320
     my_inflation = 1.02
-    my_rent = 0
-    me = income.Income(my_salary, my_salary_growth, tax1, my_save_rate, my_capital, my_misc_gain, my_capital_growth, my_living_cost, my_inflation, my_rent)
-    year = years_to_retire(my_salary, my_salary_growth, my_tax_rate, my_save_rate, my_capital, my_misc_gain, my_capital_growth, my_living_cost, my_inflation, my_rent)
+    me = income.Income(my_salary, my_salary_growth, my_save_rate, tax1, 
+                        my_capital, my_misc_gain, my_capital_growth, 
+                        my_living_cost, my_inflation)
+    year = years_to_retire(me)
     
